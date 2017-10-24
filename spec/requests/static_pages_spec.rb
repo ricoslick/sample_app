cea7f7f3633712a3fc6a52cdfc3ce92241ccc768
Ricoslick <bantuefwe@riseup.net>
2017-10-24 16:37:37 +0300

    Created Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe "Static pages" do
 		it "should have the h1 'Contact'" do
 			visit '/static_pages/contact'
 			page.should have_selector('h1', :text => 'Contact')
-		end
+		end 
 		it "should have the title 'Contact'" do
 			visit '/static_pages/contact'
 			page.should have_selector('title', :text => "#{base_title} | Contact")
