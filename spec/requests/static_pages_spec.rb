b50b022680c9c0b3361b75195f4e1ff199d9c7ed
Ricoslick <bantuefwe@riseup.net>
2017-10-24 16:27:18 +0300

    Modified static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,6 +32,16 @@ RSpec.describe "Static pages" do
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
+	describe "Contact Page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', :text => "#{base_title} | Contact")
+		end
+	end	
 end	
 
 
