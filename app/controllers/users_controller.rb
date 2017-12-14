5572c9f7f6596e37c4618b0e136103bfe95a1acc
Ricoslick <bantuefwe@riseup.net>
2017-12-14 08:35:51 +0300

    SSL in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,7 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
-  private
-
-  def user_params
-	 params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def create
+   def create
   	@user = User.new(user_params)
   	if @user.save
   		# Handle a successful save.
@@ -22,6 +16,12 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  private
+
+  def user_params
+	 params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
 
