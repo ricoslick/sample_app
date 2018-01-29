a3af33ffa88d58fea7860c5b96cb904d1e2a4c73
Ricoslick <bantuefwe@riseup.net>
2018-01-29 16:39:21 +0300

    Microposts Feed setup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -158,6 +158,18 @@ RSpec.describe User, type: :model do
 
       it { should be_following(other_user) }
       its(:followed_users) { should include(other_user) }
+
+      describe "followed user" do
+        subject { other_user }
+        its(:followers) { should include(@user)}
+      end
+
+      describe "and unfollowing" do
+        before { @user.unfollow!(other_user) }
+
+        it { should_not be_following(other_user) }
+        its(:followed_users) { should_not include(other_user) }
+      end
   end
 end
 
