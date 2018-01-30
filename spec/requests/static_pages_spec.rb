ad61ee05262a0328bc30ebf58b5ec547905bd963
Ricoslick <bantuefwe@riseup.net>
2018-01-30 16:38:45 +0300

    Followers and Followed setup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,11 +18,21 @@ RSpec.describe "Static pages" do
 				sign_in user
 				visit root_path
 			end
-
+ 
 			it "should render the user's feed" do
 				user.feed.each do |item|
 					page.should have_selector("li##{item.id}", text: item.content)
 				end
+			end
+
+			describe "follower/following counts" do
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 followers", href: followers_user_path(user)) }
 			end
 		end
 	end
